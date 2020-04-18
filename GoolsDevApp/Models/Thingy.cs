8488ff1b2cb6db33020cdcb9f4446d85b1a6a606
Eric Goolsby <egoolsby@gongos.com>
2020-04-17 22:13:42 -0400

    getting started

---
+++ GoolsDevApp/Models/Thingy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoolsDevApp.Models
+{
+    public class Thingy
+    {
+        public int Count { get; set; }
+        public string Text { get; set; }
+    }
+}
--- GoolsDevApp/Startup.cs
+++ GoolsDevApp/Startup.cs
@@ -2,12 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GoolsDevApp.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore;
+
 
 namespace GoolsDevApp
 {
@@ -24,6 +28,9 @@ namespace GoolsDevApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<GoolsDevContext>(options =>
+            options.UseMySql(Configuration.GetConnectionString("GoolsDevContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
